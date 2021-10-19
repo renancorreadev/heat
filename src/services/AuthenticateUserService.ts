ab6aff945860a31d85188c0a8f26166c46deff90
skyxcripto <producer@skynance.net>
2021-10-19 16:56:45 -0300

    Update Code & create Database

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,21 +1,37 @@
 import axios from 'axios';
+import { response } from 'express';
 /**
  * Receiver the code (string)
  * Receiver the Token Acess on the github
+ * Recover item of token github
  * Verify if user exist on DataBase
  * ------ YES = Generate one Token ========
  * ------ NO = Create a database with one Token
  * Return the token address with information of User
  */
 
+interface IAcessTokenResponse{
+    access_token: string;
+}
+
+interface IUserResponse{
+    avatar_url: string,
+    login: string,
+    id: number,
+    name: string
+}
+
 
 class AuthenticateUserService {
     //Value String typestrip
+
     async execute(code: string) {
+
     //Create acess to Github 
+
     const url = "https://github.com/login/oauth/access_token";
 
-    const response = await axios.post(url, null, { 
+    const {data : accessTokenResponse} = await axios.post<IAcessTokenResponse>(url, null, { 
         params: { 
             client_id: process.env.GITHUB_CLIENT_ID, 
             client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -25,7 +41,13 @@ class AuthenticateUserService {
             "Accept" : "application/json"
         }
     })
-    //Verify if Ok?
+
+    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+        headers: {
+            authorization: `Bearer ${accessTokenResponse.access_token} `
+        }
+    })
+
     return response.data; 
     }
 }
