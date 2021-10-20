dabfddf647e9b1a1cb204755e2a4035762315059
skyxcripto <producer@skynance.net>
2021-10-20 11:24:45 -0300

    Upload

---
+++ frontEnd/src/contexts/auth.tsx
@@ -0,0 +1,66 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+type User = {
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string;
+};
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+};
+
+const AuthContext = createContext({} as AuthContextData);
+
+type AuthProvider = {
+  children: ReactNode;
+  //ReactNode = All Componentes from React
+};
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  };
+};
+export function AuthProvider(props: AuthProvider) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const signInUrl = `https://github.com/login/oauth/authorize?/scope=user&client_id=3f6a6b74005ab0197515`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>("authenticate", {
+      //the route return type format AuthResponse
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+    localStorage.setItem("@dowhile:token", token);
+
+    setUser(user);
+  }
+  //Send data to backend
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes("?code=");
+    if (hasGithubCode) {
+      //if has code on github
+      const [urlWithoutCode, githubCode] = url.split("?code=");
+      window.history.pushState({}, "", urlWithoutCode);
+      //Replace code to empty string
+      signIn(githubCode);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ signInUrl, user }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
