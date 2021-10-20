382f86e7b98d3e6f18ded860bf42c1f4b5ff477d
skyxcripto <producer@skynance.net>
2021-10-19 21:22:42 -0300

    Update

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,8 @@
+import { prisma } from '.prisma/client';
 import axios from 'axios';
-import { response } from 'express';
+import prismaClient from "../prisma"
+import {sign} from  "jsonwebtoken"
+
 /**
  * Receiver the code (string)
  * Receiver the Token Acess on the github
@@ -48,7 +51,35 @@ class AuthenticateUserService {
         }
     })
 
-    return response.data; 
+    const {login, id, avatar_url, name} = response.data; 
+    const user = await prismaClient.user.findFirst({
+        where: {
+            github_id: id
+        }
+    })
+
+    //If  User not exist..
+    if(!user){
+        await prismaClient.user.create({
+            data: {
+                github_id: id,
+                login, 
+                avatar_url,
+                name
+            }
+        })
+    }
+
+    const token = sign({
+       name: user.name,
+       avatar_ur: user.avatar_url,
+       id: user.id
+    },
+        process.env.JWT_SECRET,{
+        subject: user.id, 
+        expiresIn: "1d"
+    })
+    return {token, user}; 
     }
 }
 
