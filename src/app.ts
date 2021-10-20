7140aa244cb39ab3d7be5e143bc000ff22eb04b7
skyxcripto <producer@skynance.net>
2021-10-19 22:17:49 -0300

    Create Server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
 import "dotenv/config";
 import * as express from 'express';
 import {router} from './routes';
+import {Server} from "socket.io"
+import * as http from 'http';
+import * as cors from "cors";
 
 const app = express();
+const ServerHttp = http.createServer(app); 
 app.use(express.json()); 
 app.use(router); 
 
+const io = new Server(ServerHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`User connect to Socket ${socket.id}`)
+});
 
 app.get("/github", (request, response) => {
     response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
@@ -16,5 +29,5 @@ app.get("/signin/callback", (request, response) => {
     return response.json(code); 
 })
 
-app.listen(4000, () => console.log(`🚀 Server is running on port 4000 `));
+ServerHttp.listen(4000, () => console.log(`🚀 Server is running on port 4000 `));
 
